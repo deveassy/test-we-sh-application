5bb605ec353e30e8e554a3aaaaa9b5e868eccad2
deveassy <xjfmwld@gmail.com>
2020-08-18 23:43:12 +0900

    BottomTabNavigator 적용 test중

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import "react-native-gesture-handler";
 import * as React from "react";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import MainScreen from "./components/MainScreen";
+import MainScreen from "./components/Main";
 import AlbumScreen from "./components/Album";
 import CalendarScreen from "./components/Calendar";
 import WishListScreen from "./components/WishList";
@@ -57,34 +59,100 @@ import LetterScreen from "./components/Letter";
 //     </View>
 //   );
 // }
+// const Stack = createStackNavigator();
 
-// function SecondScreen({ navigation }) {
+// function App() {
 //   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Second Screen</Text>
-//       <Button title="Go Back" onPress={() => navigation.goBack()} />
-//     </View>
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen
+//           name="WE'SH"
+//           component={MainScreen}
+//           // options={{ title: "WE'SH" }}
+//           // initialParams={{ itemId: 42 }}
+//         />
+//         <Stack.Screen name="Album" component={AlbumScreen} />
+//         <Stack.Screen name="Calendar" component={CalendarScreen} />
+//         <Stack.Screen name="WishList" component={WishListScreen} />
+//         <Stack.Screen name="Letter" component={LetterScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
 //   );
 // }
+// export default App;
 
-const Stack = createStackNavigator();
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen입니다. test중이에요.</Text>
+    </View>
+  );
+}
 
-function App() {
+const MainStack = createStackNavigator();
+
+function MainStackScreen() {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Main" component={MainScreen} />
+      <MainStack.Screen name="Details" component={DetailsScreen} />
+    </MainStack.Navigator>
+  );
+}
+
+const AlbumStack = createStackNavigator();
+
+function AlbumStackScreen() {
+  return (
+    <AlbumStack.Navigator>
+      <AlbumStack.Screen name="Album" component={AlbumScreen} />
+      <AlbumStack.Screen name="Details" component={DetailsScreen} />
+    </AlbumStack.Navigator>
+  );
+}
+
+const CalendarStack = createStackNavigator();
+
+function CalendarStackScreen() {
+  return (
+    <CalendarStack.Navigator>
+      <CalendarStack.Screen name="Calendar" component={CalendarScreen} />
+    </CalendarStack.Navigator>
+  );
+}
+
+const WishListStack = createStackNavigator();
+
+function WishListStackScreen() {
+  return (
+    <WishListStack.Navigator>
+      <WishListStack.Screen name="WishList" component={WishListScreen} />
+    </WishListStack.Navigator>
+  );
+}
+
+const LetterStack = createStackNavigator();
+
+function LetterStackScreen() {
+  return (
+    <LetterStack.Navigator>
+      <LetterStack.Screen name="Letter" component={LetterScreen} />
+    </LetterStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="WE'SH"
-          component={MainScreen}
-          options={{ title: "WE'SH" }}
-          initialParams={{ itemId: 42 }}
-        />
-        <Stack.Screen name="Album" component={AlbumScreen} />
-        <Stack.Screen name="Calendar" component={CalendarScreen} />
-        <Stack.Screen name="WishList" component={WishListScreen} />
-        <Stack.Screen name="Letter" component={LetterScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Main" component={MainStackScreen} />
+        <Tab.Screen name="Album" component={AlbumStackScreen} />
+        <Tab.Screen name="Calendar" component={CalendarStackScreen} />
+        <Tab.Screen name="WishList" component={WishListStackScreen} />
+        <Tab.Screen name="Letter" component={LetterStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-export default App;
--- components/Album.js
+++ components/Album.js
@@ -1,11 +1,15 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Text } from "react-native";
+import { Text, Button } from "react-native";
 
 export default function AlbumScreen({ navigation }) {
   return (
     <Container>
       <Text>Album Screen입니다.</Text>
+      <Button
+        title="Details Button"
+        onPress={() => navigation.navigate("Details")}
+      />
     </Container>
   );
 }
