2e6b17cf60e9d91a785868f67e9757426bc65a02
deveassy <xjfmwld@gmail.com>
2020-08-19 23:53:46 +0900

    코드정리, 탭 아이콘 테스트중

--- App.js
+++ App.js
@@ -4,82 +4,13 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import MainScreen from "./components/Main";
 import AlbumScreen from "./components/Album";
 import CalendarScreen from "./components/Calendar";
 import WishListScreen from "./components/WishList";
 import LetterScreen from "./components/Letter";
-
-// function HomeScreen({ navigation }) {
-//   return (
-//     <View
-//       style={{
-//         flex: 1,
-//         alignItems: "center",
-//         justifyContent: "center",
-//       }}
-//     >
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Go to Details Screen"
-//         onPress={() =>
-//           navigation.navigate("First", {
-//             itemId: 1,
-//             otherParam: "anything you want here",
-//           })
-//         }
-//       />
-//       <Button
-//         title="Go to Second Screen"
-//         onPress={() => navigation.navigate("Second")}
-//       />
-//     </View>
-//   );
-// }
-
-// function AlbumScreen({ navigation }) {
-//   // const { itemId } = route.params;
-//   // const { otherParam } = route.params;
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>Album Screen</Text>
-//       {/* <Text>itemId: {JSON.stringify(itemId)}</Text>
-//       <Text>otherParam: {JSON.stringify(otherParam)}</Text> */}
-//       <Button
-//         title="Go to Details... again"
-//         onPress={() =>
-//           navigation.push("Details", {
-//             itemId: Math.floor(Math.random() * 100),
-//           })
-//         }
-//       />
-//       <Button title="Go to Home" onPress={() => navigation.navigate("WE'SH")} />
-//       <Button title="Go Back" onPress={() => navigation.goBack()} />
-//     </View>
-//   );
-// }
-// const Stack = createStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           name="WE'SH"
-//           component={MainScreen}
-//           // options={{ title: "WE'SH" }}
-//           // initialParams={{ itemId: 42 }}
-//         />
-//         <Stack.Screen name="Album" component={AlbumScreen} />
-//         <Stack.Screen name="Calendar" component={CalendarScreen} />
-//         <Stack.Screen name="WishList" component={WishListScreen} />
-//         <Stack.Screen name="Letter" component={LetterScreen} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-// export default App;
 
 function DetailsScreen() {
   return (
@@ -145,8 +76,17 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
+    <NavigationContainer style={{ backgroundColor: "#999" }}>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Main") {
+              iconName = focused ? "" : "";
+            }
+          },
+        })}
+      >
         <Tab.Screen name="Main" component={MainStackScreen} />
         <Tab.Screen name="Album" component={AlbumStackScreen} />
         <Tab.Screen name="Calendar" component={CalendarStackScreen} />
--- components/Main.js
+++ components/Main.js
@@ -3,42 +3,14 @@ import styled from "styled-components";
 import { ImageBackground, Button } from "react-native";
 
 export default function MainScreen({ navigation }) {
-  //   const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
-  //   if (betweenTimeDay < 365) {
-  //     return `${betweenTimeDay}일전`;
-  //   }
   return (
     <Container>
-      {/* <ImageBackground
-        style={{ width: "100%", height: "100%" }}
-        source={require("/album.png")}
-        resizeMode="cover"
-      > */}
       <MainText>WE'SH의 메인스크린입니다.</MainText>
       <MainText>마땅한 커플사진을 아직 찾지 못했어요...ㅠㅠ</MainText>
       <Button
         title="Details Button"
         onPress={() => navigation.navigate("Details")}
       />
-      {/* <MainText>{betweenTimeDay}</MainText> */}
-      {/* </ImageBackground> */}
-      {/* <ButtonContainer>
-        <MenuButton onPress={() => navigation.navigate("Main")}>
-          <ButtonText>Main</ButtonText>
-        </MenuButton>
-        <MenuButton onPress={() => navigation.navigate("Album")}>
-          <ButtonText>ALBUM</ButtonText>
-        </MenuButton>
-        <MenuButton onPress={() => navigation.navigate("Calendar")}>
-          <ButtonText>CALENDAR</ButtonText>
-        </MenuButton>
-        <MenuButton onPress={() => navigation.navigate("WishList")}>
-          <ButtonText>WISH LIST</ButtonText>
-        </MenuButton>
-        <MenuButton onPress={() => navigation.navigate("Letter")}>
-          <ButtonText>LOVE LETTE</ButtonText>
-        </MenuButton>
-      </ButtonContainer> */}
     </Container>
   );
 }
@@ -48,28 +20,9 @@ const Container = styled.View`
   position: relative;
   align-items: center;
   justify-content: center;
-  /* background-color: #faf3dd; */
+  background-color: #faf3dd;
 `;
 
 const MainText = styled.Text`
   font-size: 15px;
-`;
-
-const ButtonContainer = styled.View`
-  position: absolute;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  bottom: 0;
-  border: 1px solid #000;
-  padding: 5px;
-`;
-const MenuButton = styled.TouchableOpacity`
-  margin: 5px;
-  padding: 5px;
-  border: 3px solid tomato;
-`;
-const ButtonText = styled.Text`
-  font-size: 14px;
 `;
