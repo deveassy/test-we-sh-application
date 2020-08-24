fe1a8729cae2522033e0252f6ed51b5c665a03bf
deveassy <xjfmwld@gmail.com>
2020-08-24 23:47:09 +0900

    calendar screen 구현 테스트중

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,15 +1,98 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Text, Button } from "react-native";
+import {
+  Calendar,
+  CalendarList,
+  Agenda,
+  LocaleConfig,
+} from "react-native-calendars";
 
-export default function CalendarScreen({ navigation }) {
+LocaleConfig.locales["en"] = {
+  monthNames: [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ],
+  monthNamesShort: [
+    "jan",
+    "feb",
+    "mar",
+    "apr",
+    "may",
+    "jun",
+    "jul",
+    "aug",
+    "sep",
+    "oct",
+    "nov",
+    "dec",
+  ],
+  dayNames: [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ],
+  dayNamesShort: ["S", "M", "T", "W", "T", "F", "S"],
+  today: "Aujourd'hui",
+};
+
+LocaleConfig.defaultLocale = "en";
+
+export default function CalendarScreen() {
   return (
     <Container>
-      <Text>Calendar Screen입니다.</Text>
-      <Button
-        title="Details Button"
-        onPress={() => navigation.navigate("Details")}
-      />
+      <CalendarContainer>
+        <Calendar
+          current={"2020-08-24"}
+          minDate={"2020-01-01"}
+          maxDate={"2020-12-31"}
+          onDayPress={(day) => {
+            console.log("selected day", day);
+          }}
+          onDayLongPress={(day) => {
+            console.log("selected day", day);
+          }}
+          monthFormat={"yyyy MM"}
+          onMonthChange={(month) => {
+            console.log("month changed", month);
+          }}
+          hideArrows={true}
+          renderArrow={(direction) => <Arrow />}
+          hideExtraDays={false}
+          disableMonthChange={true}
+          firstDay={7}
+          hideDayNames={false}
+          showWeekNumbers={false}
+          onPressArrowLeft={(substractMonth) => substractMonth()}
+          onPressArrowRight={(addMonth) => addMonth()}
+          disableArrowLeft={false}
+          disableArrowRight={false}
+          disableAllTouchEventsForDisabledDays={true}
+          markedDates={{
+            "2020-08-16": {
+              selected: true,
+              marked: true,
+              selectedColor: "skyblue",
+            },
+            "2020-08-17": { marked: true },
+            "2020-08-18": { marked: true, dotColor: "red", activeOpacity: 0 },
+            // "2020-08-19": { disabled: true, disableTouchEvent: true },
+          }}
+        />
+      </CalendarContainer>
     </Container>
   );
 }
@@ -18,4 +101,9 @@ const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+`;
+
+const CalendarContainer = styled.View`
+  flex: 1;
+  padding-top: 80px;
 `;
