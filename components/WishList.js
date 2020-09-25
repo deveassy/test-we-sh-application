03a421f7fe6a562a612698c2194f2f5b63fb717d
deveassy <xjfmwld@gmail.com>
2020-09-25 23:59:14 +0900

    WishList screen 스타일링 중
    최적의 방법으로 사용할 수 있도록 여러가지 시도해보는 중

--- components/WishList.js
+++ components/WishList.js
@@ -1,34 +1,97 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Text, Button } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
+import { Text, Button, InputText } from "react-native";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
 
 export default function WishListScreen({ navigation }) {
   return (
     <Container>
       <ListContainer>
-        <AntDesign name="checkcircleo" size={25} />
+        <Ionicons
+          name="ios-rocket"
+          size={25}
+          style={{ marginLeft: 10, marginRight: 5 }}
+        />
         <ListItem
           title="하동 짚라인"
           onPress={() => navigation.navigate("Details")}
-          color="#000"
+          color="#b52b65"
         ></ListItem>
+        <AntDesign
+          name="checkcircleo"
+          size={25}
+          style={{ position: "absolute", right: 10 }}
+        />
       </ListContainer>
       <ListContainer>
-        <AntDesign name="checkcircleo" size={25} />
+        <Ionicons
+          name="ios-cafe"
+          size={25}
+          style={{ marginLeft: 10, marginRight: 5 }}
+        />
         <ListItem
-          title="Wish List Screen입니다."
-          onPress={() => navigation.navigate("Details")}
-          color="#000"
+          title="장기동 스우스 커피"
+          // onPress={() => navigation.navigate("Details")}
+          color="#6f4a8e"
         ></ListItem>
+        <AntDesign
+          name="checkcircleo"
+          size={25}
+          style={{ position: "absolute", right: 10 }}
+        />
       </ListContainer>
       <ListContainer>
-        <AntDesign name="checkcircleo" size={25} />
+        <Ionicons
+          name="logo-model-s"
+          size={25}
+          style={{ marginLeft: 10, marginRight: 5 }}
+        />
         <ListItem
-          title="Wish List Screen입니다."
-          onPress={() => navigation.navigate("Details")}
-          color="#000"
+          title="삼척 씨스포빌 카라반캠핑"
+          // onPress={() => navigation.navigate("Details")}
+          color="#62760c"
         ></ListItem>
+        <AntDesign
+          name="checkcircleo"
+          size={25}
+          style={{ position: "absolute", right: 10 }}
+        />
+      </ListContainer>
+      <ListContainer>
+        <Ionicons
+          name="md-appstore"
+          size={25}
+          style={{ marginLeft: 10, marginRight: 5 }}
+        />
+        <ListItem
+          title="테넷"
+          // onPress={() => navigation.navigate("Details")}
+          color="#f08a5d"
+        ></ListItem>
+        <AntDesign
+          name="checkcircleo"
+          size={25}
+          style={{ position: "absolute", right: 10 }}
+        />
+      </ListContainer>
+      <ListContainer>
+        <Ionicons
+          name="ios-cafe"
+          size={25}
+          style={{ marginLeft: 10, marginRight: 5 }}
+        />
+        <ListItem
+          title="크리스피도넛 글레이즈드"
+          // onPress={() => navigation.navigate("Details")}
+          color="#3b6978"
+        >
+          <InputText />
+        </ListItem>
+        <AntDesign
+          name="checkcircleo"
+          size={25}
+          style={{ position: "absolute", right: 10 }}
+        />
       </ListContainer>
     </Container>
   );
@@ -46,11 +109,11 @@ const Container = styled.View`
 const ListContainer = styled.View`
   flex-direction: row;
   align-items: center;
+  position: relative;
   padding: 10px 0;
   border-bottom-width: 1px;
 `;
 
 const ListItem = styled.Button`
-  /* margin: 7px 0 7px 10px; */
   font-size: 20px;
 `;
